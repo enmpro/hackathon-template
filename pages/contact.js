499bb5a19250a08a1e99841cc3f61e1b01974634
Enmanuel Proano <rbsrman123@gmail.com>
2022-10-26 10:34:35 -0400

    removed images from contact, contact slide instead

--- pages/contact.js
+++ pages/contact.js
@@ -1,15 +1,9 @@
 // when the page shows, call the functions
 window.onpageshow = function() {
     scrollFade()
-    scrollFadeOther()
 };
 
 // changes the element class to slideUp/slideUpOther to make the animation run
 function scrollFade() {
     document.getElementById("profile-pic").className = "slideUp";
-}
-function scrollFadeOther() {
-    document.getElementById("c++").className = "slideUpOther";
-    document.getElementById("java").className = "slideUpOther";
-    document.getElementById("python").className = "slideUpOther";
 }
