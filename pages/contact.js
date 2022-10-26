5d964ef7a85a5a6694d0b25e8c8e2c9d537f13a0
Enmanuel Proano <rbsrman123@gmail.com>
2022-10-25 20:42:06 -0400

    Continued edits, added javascript animations

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,10 @@
+window.onpageshow = function() {scrollFade()};
+window.onpageshow = function() {scrollFadeOther()};
+
 function scrollFade() {
     
+    document.getElementById("profile-pic").className = "slideUp";
+}
+function scrollFadeOther() {
+    document.getElementById("c++").className = "slideUpOther";
 }
